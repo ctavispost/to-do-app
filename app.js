cb916190b59d6e392a6e30c61b7f7d58ff4bfd0d
ctavispost <ctavispost@gmail.com>
2019-06-06 19:24:24 -0700

    Progress in assignment

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 function onReady() {
-  let toDoId = 0;
-  let toDos = [];
+
+  //retreive local Storage and return list to toDos array and set counter, or reset array and coutner
+  const toDoMemory = localStorage.getItem('toDos');
+  console.log(toDoMemory);
+  console.log(toDoMemory === null);
+  
+  if (toDoMemory === null) {
+    let toDoId = 0;
+    let toDos = [];
+  } else {
+    let toDos = JSON.parse(toDoMemory);
+    let toDoId = toDos.length;
+  }
 
   const addToDoForm = document.getElementById('addToDoForm');
 
@@ -11,11 +22,24 @@ function onReady() {
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
+
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
 
+      //listen for checkbox change and set truth value for complete
+      checkbox.addEventListener( 'change', function() {
+        if(this.checked) {
+          toDo.complete = true;
+        } else {
+          toDo.complete = false;
+        }
+        console.log(toDo.complete);
+      });
+
+
+      //delete button removes list items
       const deleteButton = document.createElement('button');
-      deleteButton.textContent = "Delete";
+      deleteButton.textContent = 'Delete';
 
       deleteButton.addEventListener('click', () => {
         toDos = toDos.filter(function(item) {
@@ -42,6 +66,8 @@ function onReady() {
       complete: false,
       id: toDoId
     });
+
+    localStorage.setItem('toDos', JSON.stringify(toDos));
 
     newToDoText.value = '';
     toDoId++;
